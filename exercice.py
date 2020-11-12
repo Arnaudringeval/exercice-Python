4251c993214abce35758669b29dda5b4094a1bdd
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2020-11-12 15:33:15 +0100

    jeudiv5

--- exercice.py
+++ exercice.py
@@ -13,3 +13,9 @@ while i < 10:
     i+= 1
     if i <= 10:
         print(i,"x",table,"=",i * table)
+
+table2 = int(input("De Quelle nombre voulez vous la table de multiplication?"))
+
+
+for i in range(1,11):
+    print(i,"x",table2,"=",i * table2)
