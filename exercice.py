53a63d2ce551eaa19cae9666ca1edeb8a3f1b9af
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2020-11-12 12:11:44 +0100

    jeudi

---
+++ exercice.py
@@ -0,0 +1,15 @@
+age = int(input("Quel est votre age?"))
+
+if age > 18:
+    print("Vous êtes majeur")
+
+else:
+    print("Vous êtes mineur")
+
+table = int(input("De Quelle nombre voulez vous la table de multiplication?"))
+
+i = 0
+while i < 10:
+    i+= 1
+    if i < 11:
+        print(i * table)
